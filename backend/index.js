2235972bfa3ccfcaaaf993e276d19fdf0fb6f6a3
unknown <teammadway@gmail.com>
2026-01-21 13:23:44 +0530

    Changes IN COrs Policy

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,38 @@ app.use("/api/client", clientRoutes);
 
 // Middlewares 
 
-app.use(cors());
+// app.use(cors());
+
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://wortham-project.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow server-to-server or Postman
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
+
+
+// app.use(cors(...));
+app.options("*", cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
